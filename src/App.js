b5b9700a804d5abd5dd0582449d4922516cde214
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-05-25 03:26:52 +0200

    home,about,nav section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/NavBar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
-      <h1>  Hello</h1>
+        <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/about" element={<About></About>} />
+      </Routes>
     </div>
   );
 }
